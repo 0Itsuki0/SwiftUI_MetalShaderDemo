64f767752d4f0fcdb1e99a9b021739f99075fb03
itsuki <itsuki.enjyoy@gmail.com>
2025-08-16 10:23:51 +0900

    Initial Commit

---
+++ MetalDemo/MetalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalDemoApp.swift
+//  MetalDemo
+//
+//  Created by Itsuki on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct MetalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
